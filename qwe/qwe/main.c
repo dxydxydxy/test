15f6fc7a074f8d01f95f3383713dbc0c427c4ae5
lk <746401552@qq.com>
2016-01-08 16:27:29 +0800

    nn

---
+++ qwe/qwe/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  qwe
+//
+//  Created by lk on 16/1/8.
+//  Copyright © 2016年 lk. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("121");
+    return 0;
+}
